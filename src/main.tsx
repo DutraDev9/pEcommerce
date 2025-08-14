85191c9391df7d47f658aea2dce938bead547215
devdutra <dutradev9@gmail.com>
2025-08-14 09:27:08 -0300

    Ecommerce Project

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { router }from './App.tsx'
+import { RouterProvider } from 'react-router-dom'
+import './index.css'
+
+import { Toaster} from 'react-hot-toast'
+import CartProvider from './contexts/CartContext.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <CartProvider>
+      <Toaster
+            position="top-center"
+            reverseOrder={false}
+          />
+    <RouterProvider router={router}/>
+    </CartProvider>
+  </StrictMode>,
+)
